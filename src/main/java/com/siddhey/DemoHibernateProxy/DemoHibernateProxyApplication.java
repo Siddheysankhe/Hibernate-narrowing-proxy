4f6e7bdb979e82a7b183fde146b2346530fc4bac
siddhey <siddhey.sankhe@kyepot.com>
2018-10-27 20:28:46 +0530

    first commit

---
+++ src/main/java/com/siddhey/DemoHibernateProxy/DemoHibernateProxyApplication.java
@@ -0,0 +1,12 @@
+package com.siddhey.DemoHibernateProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHibernateProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHibernateProxyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/siddhey/DemoHibernateProxy/DemoHibernateProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.siddhey.DemoHibernateProxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoHibernateProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
